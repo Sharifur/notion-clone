bddcf48cf4f88474d273d249ef08b7af3bf17833
sharifur <rsharifur824@gmail.com>
2023-10-24 14:03:55 +0600

    markting page ui in progress

---
+++ app/(marketing)/_components/Footer.tsx
@@ -0,0 +1,17 @@
+
+import {Logo} from "./Logo";
+import {Button} from "@/components/ui/button";
+
+export const Footer = () => {
+    return (
+        <div className="flex items-center w-full p-6 bg-background z-20">
+            <Logo/>
+            <div className="md:ml-auto
+            w-full justify-between md:justify-end flex items-center gap-x-2 text-muted-foreground
+            ">
+                <Button variant="ghost" size="sm">Privacy Poloicy</Button>
+                <Button variant="ghost" size="sm">Terms & Conditions</Button>
+            </div>
+        </div>
+    )
+}
