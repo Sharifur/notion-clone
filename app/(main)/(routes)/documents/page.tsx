a341bb502d27d334c4bc8c37ced3e7ec00bc806c
Sharifur Rahman <rsharifur824@gmail.com>
2023-10-26 11:04:36 +0600

    middleware added in jotion main pages

---
+++ app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,10 @@
+const DocumentsPage = () => {
+    return (
+        <div className="div">
+            this is a protected DocumentsPage
+        </div>
+    )
+}
+
+export default DocumentsPage;
+
