4c6b24a3afdc76d4789af3589a64a702975de095
Sharifur Rahman <rsharifur824@gmail.com>
2023-10-30 10:59:00 +0600

    button component renamcaed

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,8 +1,35 @@
-const DocumentPageId = () => {
+"use client";
+
+import { Toolbar } from "@/components/Toolbar";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
+
+interface DocumentPageIdProps {
+    params: {
+        documentId : Id<"documents">
+    }
+}
+
+const DocumentPageId = ({params} : DocumentPageIdProps) => {
+
+    const document = useQuery(api.documents.getById,{
+        documentId : params.documentId
+    });
+
+    if(document === undefined) {
+        return (
+            <p>Loading...</p>
+        )
+    }
+
     return ( 
-        <>
-            Document Id
-        </>
+        <div className="pb-40">
+            <div className="h-[35vh]"/>
+            <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+                <Toolbar initialData={document}/>
+            </div>
+        </div>
      );
 }
  
